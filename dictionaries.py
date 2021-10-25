eaac3925392ac9342471634c9b5b7c83f6b5b8b1
emilymorgenbesser <morgenbesseremily@gmail.com>
2021-10-25 10:48:25 -0400

    fizzbuzz

---
+++ dictionaries.py
@@ -0,0 +1,20 @@
+# word = key, value = definition
+
+# creating a dictionary
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "July",
+    "Aug": "August",
+    "Sept": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+# accessing a specific key or value
+# print(monthConversions["Nov"])
+birthday_month = print(monthConversions["Nov"])
---
+++ while-loops.py
@@ -0,0 +1,10 @@
+i = 1
+while i <= 10:
+        print(i)
+        i += 1
+print("done with loop")
+
+i = 1
+while i <= 100:
+         print("fizz")
+
